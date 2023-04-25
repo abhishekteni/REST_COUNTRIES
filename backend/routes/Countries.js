408e3dfa28984227ea010ccd957cef0425fc5f9b
abhishek teni <abhishekteni@rediffmail.com>
2023-04-25 23:52:02 +0100

    fetch api

---
+++ backend/routes/Countries.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const axios = require('axios');
+const router  = express.Router()
+
+router.get('/',async(req,res)=>{
+    try {
+        const response = await axios.get('https://restcountries.com/v3.1/all');
+        res.json(response.data);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal server error');
+      }
+})
+
+router.get('/countryname/:name',async(req,res)=>{
+  const name = req.params.name;
+  try {
+    const response = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  }
+})
+
+
+router.get('/countryregion/:region',async(req,res)=>{
+  const region = req.params.region;
+  try {
+    const response = await axios.get(`https://restcountries.com/v3.1/region/${region}`);
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  }
+})
+
+module.exports=router
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const countryRoutes = require('./routes/Countries')
+require('dotenv').config()
+// express app
+const app = express()
+
+
+app.use(express.json())
+// listen for request 
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+// routes
+app.use('/api/countries',countryRoutes)
+
+app.listen(process.env.PORT,()=>{
+    console.log('listening on port 4000')
+})
+
